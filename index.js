13130162e0d4db171535efaec88c6e86a5321dd9
Dan Jones <djones109+c9@gmail.com>
2018-04-13 17:58:15 +0000

    💾🎉 Add authorization

--- index.js
+++ index.js
@@ -13,14 +13,16 @@ const PORT = process.env.PORT || 8080;
 
 const args = require('minimist')(process.argv.slice(2), {
     boolean: ['help'],
-    string: ['port', 'folder'],
+    string: ['port', 'folder', 'token'],
     alias: {
         help: ['h'],
         port: ['p'],
+        token: ['t'],
         folder: ['directory', 'd', 'f']
     },
     "default": {
         port: PORT,
+        token: process.env.BEARER_TOKEN,
         folder: 'Journal'
     }
 });
@@ -34,8 +36,11 @@ if (args.help) {
     --help, -h                     Print this help
     --port, -p                     Port to run the server (defaults to ${PORT})
     --folder, -f, --directory, -d  Directory to serve, defaults to ./Journal
+    --token, -t                    Bearer token to check from header
 
   If no port is specified, it will use the PORT environment variable, or 8080 otherwise.
+
+  If no token is specified, it will use the BEARER_TOKEN variabble. If that's not specified, no security will be used.
     `);
     process.exit();
 }
@@ -43,6 +48,21 @@ if (args.help) {
 const express = require('express');
 const app = express();
 const readdir = Promise.promisify(fs.readdir);
+
+if (args.token) {
+    app.use(function(req, res, next) {
+        let auth = req.get('Authorization') || '';
+        let isBearer = auth.substr(0, 7) === 'Bearer ';
+        let bearer = auth.substr(7);
+
+        if (isBearer && bearer == args.token) {
+            next();
+            return;
+        }
+
+        res.status(403).send('');
+    });
+}
 
 let filesPromise = readdir(args.folder).catch(function (err) {
     console.error(err.message);
